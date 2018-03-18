66b439444ca1d368725e0dc634be927862f92d54
Tucker <takahirosuzuki.m010@gmail.com>
2018-03-18 23:02:52 +0900

    Login/out with token, store the date into the Local db

--- angular/src/app/components/login/login.component.ts
+++ angular/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';  //#So that we can redirect
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  
+    //#To add username and password as properties
+  username: String;
+  password: String;
+  
+    //#To use the survices imported from other folder we need to inject it
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit(){
+    console.log(this.username);
+      //#When user data are submitted, this function creates the object below
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+      //#To take above objects and submit it through the authenticate service to the backend authenticate route
+      this.authService.authenticateUser(user).subscribe(data => {
+      //#To store the authenticateUser data once authenticateUser function make the POST request successful, 
+      //that function returns the token for us to store and it also returns user info.
+      //#So once authenticateUser function make success we need to store them with into the local storage with below function
+    console.log(data);
+    if (data.success) {
+      //#If user enter the registered user name and pw, then, store the user data and its token into the local db storage
+      this.authService.storeUserData(data.token, data.user);
+      this.flashMessage.show('You are now logged in', {
+        cssClass: 'alert-success', 
+        timeout: 5000
+      });
+      this.router.navigate(['dashboard']);
+    } else {
+      this.flashMessage.show(data.msg, {
+        cssClass: 'alert-danger', 
+        timeout: 5000
+      });
+      this.router.navigate(['login']);  //#To make it redirect the login page
+    }
+   });
+
   }
 
 }
--- angular/src/app/services/auth.service.ts
+++ angular/src/app/services/auth.service.ts
@@ -11,16 +11,39 @@ export class AuthService {
 
   constructor(
     private _http: HttpClient, 
-   // private response: Response
+  // private response: Response
   ) { }
 
-  //#This function makes backend API possible to register the POST request
-  //registerUser(user) {
+    //#This function makes backend API possible to register the POST request
+    //registerUser(user) {
   registerUser(user): any{
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');   //#To append(attach) content type JSON to headers, send the content type to application/json
     return this._http.post('http://localhost:3000/users/register', user, {headers: headers});
       //.map(res => res.json());
-      //.map((res: Response) => res.json());
+      //.map((res: Response) => res.json());       //#See the Error on the README, JSON is an assumed default and no longer needs to be explicitly parsed in HttpClient Angular2
   } 
+   
+     //#To create authenticate user service
+   authenticateUser(user){
+       //#Make a POST request to authenticate
+     let headers = new HttpHeaders();
+     headers = headers.append('Content-Type', 'application/json');
+     return this._http.post('http://localhost:3000/users/authenticate', user, {headers: headers});
+   }
+
+      //#TO create storeUserData function
+    storeUserData(token, user){
+      localStorage.setItem('id_token', token);
+      localStorage.setItem('user', JSON.stringify(user));  //Local storage can only store string, cannot store an object
+      this.authToken = token;
+      this.user = user;
+    }
+
+      //#To create Logout
+    logout(){
+      this.authToken = null;
+      this.user = null;
+      localStorage.clear();
+    }
 }
